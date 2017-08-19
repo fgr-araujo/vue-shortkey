5e1471884c9bbf1ec1ac128b708b0223e4909e09
iFgR <ifgr.fagner@gmail.com>
2017-08-19 09:58:38 -0300

    Add some functional tests && sinon

--- test/functional.test.js
+++ test/functional.test.js
@@ -5,23 +5,26 @@ import Shortkey from '../src/index.js'
 
 find.shim()
 
+const VM = pTemplate => new Vue({
+  template: pTemplate,
+  data() {
+    return {
+      called: false
+    }
+  },
+  methods: {
+    foo() {
+      this.called = true
+    }
+  }
+})
+
+
 describe('functionnal tests', () => {
   Vue.use(Shortkey)
 
   it('listen for keydown and dispatch event', () => {
-    const vm = new Vue({
-      template: '<div @shortkey="foo" v-shortkey="[\'q\']"></div>',
-      data() {
-        return {
-          called: false
-        }
-      },
-      methods: {
-        foo() {
-          this.called = true
-        }
-      }
-    })
+    const vm = new VM('<div @shortkey="foo" v-shortkey="[\'q\']"></div>')
     vm.$mount()
 
     var keydown = document.createEvent('HTMLEvents')
@@ -38,20 +41,9 @@ describe('functionnal tests', () => {
     const div = document.createElement('div')
     document.body.appendChild(div)
 
-    const vm = new Vue({
-      template: '<div @shortkey="foo" v-shortkey="[\'b\']"><textarea v-shortkey.avoid></textarea></div>',
-      data() {
-        return {
-          called: false
-        }
-      },
-      methods: {
-        foo() {
-          this.called = true
-        }
-      }
-    })
+    const vm = new VM('<div @shortkey="foo" v-shortkey="[\'b\']"><textarea v-shortkey.avoid></textarea></div>')
     vm.$mount(div)
+
     const textarea = vm.$el.querySelector('textarea')
     textarea.focus()
     expect(document.activeElement == textarea).to.be.true
@@ -64,5 +56,43 @@ describe('functionnal tests', () => {
 
     expect(vm.called).to.be.false
     vm.$destroy()
+  })
+
+  it('Setting focus with .focus modifier', () => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+
+    const vm = new VM(`<div><input type="text" /> <button type="button" v-shortkey.focus="['f']">BUTTON</button></div>`)
+    vm.$mount(div)
+
+    const inputText = vm.$el.querySelector('input')
+    inputText.focus()
+    expect(document.activeElement == inputText).to.be.true
+
+    let keydown = document.createEvent('HTMLEvents')
+    keydown.initEvent('keydown', false, true)
+    keydown.key = 'f'
+    document.dispatchEvent(keydown)
+
+    const buttonInput = vm.$el.querySelector('button')
+    expect(document.activeElement == buttonInput).to.be.true
+  })
+
+  it.only('Bring push button with .push modifier', () => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+
+    const vm = new VM(`<div><button type="button" v-shortkey.push="['p']">BUTTON</button></div>`)
+    vm.$mount(div)
+
+    let spyFoo = sinon.spy(vm, 'foo')
+
+    let keydown = document.createEvent('HTMLEvents')
+    keydown.initEvent('keydown', false, true)
+    keydown.key = 'p'
+    document.dispatchEvent(keydown)
+
+    expect(spyFoo).to.be.calledTwice
+    spyFoo.restore()
   })
 })
