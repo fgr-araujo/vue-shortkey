23d0022006e326bd9b6035e4af1cb5666e2f24d6
Marc Dix <mdix@users.noreply.github.com>
2019-04-24 14:47:50 +0200

    IE11: Object doesn't support this action - Event constructor fix (#80)
    * adding custom event polyfill for IE11 compatibility
    
    * using custom event

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'element-matches';
+import 'custom-event-polyfill';
 
 let ShortKey = {}
 let mapFunctions = {}
@@ -110,7 +111,7 @@ const createShortcutIndex = (pKey) => {
 }
 
 const dispatchShortkeyEvent = (pKey) => {
-  const e = new Event('shortkey', { bubbles: false })
+  const e = new CustomEvent('shortkey', { bubbles: false })
   if (mapFunctions[pKey].key) e.srcKey = mapFunctions[pKey].key
   const elm = mapFunctions[pKey].el
   elm[elm.length - 1].dispatchEvent(e)
